e03c4ab6f954cc19c1e255bdc354bd2bdf1e0f16
Farshad Kooti <68573929+farshadkt87@users.noreply.github.com>
2021-06-23 11:10:35 -0700

    adding copyright

--- utils.R
+++ utils.R
@@ -1,3 +1,7 @@
+# Utils Functions
+# Notebook author: Martin Saveski (msaveski@mit.edu)
+# Copyright (c) Facebook, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the LICENSE file in the root directory of this source tree.
 
 #################################################################################
 #
