a7731881a5624523a906e34cef1cece4cee0dfdb
Farshad Kooti <68573929+farshadkt87@users.noreply.github.com>
2021-06-23 10:23:24 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,28 @@
+# Social Catalysts: Characterizing People Who Spark Conversations Among Others
+
+[Martin Saveski](http://martinsaveski.com/), Farshad Kooti, Sylvia Morelli Vitousek, Carlos Diuk, Bryce Bartlett, and [Lada Adamic](http://www.ladamic.com/). 
+
+This repository contains replication code for the paper, "Social Catalysts: Characterizing People Who Spark Conversations Among Others" published in CSCW'21.
+
+## List of files
+* `post_matching.ipynb` - code for matching catalyst to non-catalyst posts posted by users with the same number of friends.
+* `user_matching.ipynb` - code for matching catalyst to non-catalyst users who posted the same number of posts and the closest number of friends.
+* `analysis.ipynb` - code to replicate all figures in the paper.
+
+## Cite as
+```
+@article{saveski2021catalysts,
+    author = {Saveski, Martin and Kooti, Farshad and Morelli Vitousek, Sylvia and Diuk, Carlos and Bartlett, Bryce and Adamic, Lada},
+    title = {Social Catalysts: Characterizing People Who Spark Conversations Among Others},
+    journal = {Proc. ACM Hum.-Comput. Interact.},
+    year = {2021},
+    volume = {5},
+    number = {CSCW1},
+    articleno = {20},
+    doi = {10.1145/3434179},
+    publisher = {Association for Computing Machinery}    
+}
+```
+
+## License
+This analysis code is licensed under the MIT license found in the LICENSE file.
---
+++ utils.R
@@ -0,0 +1,66 @@
+
+#################################################################################
+#
+# Utils
+#
+#################################################################################
+
+multi_spread <- function(df, key, value) {
+  # quote key
+  keyq <- rlang::enquo(key)
+  # break value vector into quotes
+  valueq <- rlang::enquo(value)
+  s <- rlang::quos(!!valueq)
+  df %>% gather(variable, value, !!!s) %>%
+    unite(temp, !!keyq, variable) %>%
+    spread(temp, value)
+}
+
+#################################################################################
+
+per_change_delta <- function(
+  m.test, m.control,
+  n.test, n.control,
+  std.test, std.control
+) {
+  
+  se.test <- std.test / sqrt(n.test)
+  
+  se.control <- std.control / sqrt(n.control)
+  
+  mean <- (m.test - m.control) / abs(m.control) -
+    se.control ^ 2 * m.test / abs(m.control) ^ 3
+  
+  var <- (se.test / abs(m.control)) ^ 2 +
+    (se.control * abs(m.test)) ^ 2 / abs(m.control) ^ 4
+  
+  z95 <- stats::qnorm(1 - (1 - 0.95) / 2, 0, 1)
+  
+  return(data.frame(
+    mean     = mean,
+    lower95  = mean - z95 * sqrt(var),
+    upper95  = mean + z95 * sqrt(var)
+  ))
+}
+
+per_change_delta_se <- function(
+  m.test, m.control,
+  se.test, se.control
+) {
+  
+  mean <- (m.test - m.control) / abs(m.control) -
+    se.control ^ 2 * m.test / abs(m.control) ^ 3
+  
+  var <- (se.test / abs(m.control)) ^ 2 +
+    (se.control * abs(m.test)) ^ 2 / abs(m.control) ^ 4
+  
+  z95 <- stats::qnorm(1 - (1 - 0.95) / 2, 0, 1)
+  
+  return(data.frame(
+    mean     = mean,
+    lower95  = mean - z95 * sqrt(var),
+    upper95  = mean + z95 * sqrt(var)
+  ))
+}
+
+# END
